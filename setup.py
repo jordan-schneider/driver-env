6ba54ad9d606b08807e160fd9108bfa0026f8536
Jordan Schneider <jordan.jack.schneider@gmail.com>
2021-04-10 19:44:18 -0700

    Adds all packages to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     version="0.1",
     packages=[
         "driver",
+        "driver.gym",
+        "driver.car",
+        "driver.legacy",
+        "driver.planner",
     ],
     install_requires=["numpy", "gym", "matplotlib", "pyglet>=1.4", "moviepy", "tensorflow>=2.0"],
     package_data={

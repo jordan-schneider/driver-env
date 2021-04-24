be45492de5fef781eaec5615364caf0673107f59
Jordan Schneider <jordan.jack.schneider@gmail.com>
2021-04-24 08:21:35 -0700

    Adds options to legacy env to return time remaining as part of state. Adds legacy env variant that randomly resets the reward weights every episode (surfacing those weights in info).

--- driver/gym/legacy_env.py
+++ driver/gym/legacy_env.py
@@ -1,4 +1,4 @@
-from typing import Dict, Final, Tuple
+from typing import Dict, Final, Tuple, Union
 
 import numpy as np
 from driver.car.fixed_plan_car import LegacyPlanCar
@@ -7,19 +7,39 @@ from driver.gym.car_env import CarEnv
 from driver.world import ThreeLaneCarWorld
 
 import gym  # type: ignore
-from gym.spaces.box import Box  # type: ignore
+import gym.spaces  # type: ignore
+
+from ..math_utils import safe_normalize
 
 
-class LegacyEnv(CarEnv):
+class LegacyEnv:
     HORIZON: Final[int] = 50
 
-    def __init__(self, reward: np.ndarray, random_start: bool = False):
+    def __init__(self, reward: np.ndarray, random_start: bool = False, time_in_state: bool = False):
+        """Attempts to replicate legacy/gym.py as closely as possible while using tf ops.
+
+        Args:
+            reward (np.ndarray): Weights of linear reward features
+            random_start (bool, optional): Should the environment start at a random state after reset? Defaults to False.
+            time_in_state (bool, optional): Should the state include how long is left in the episode? Defaults to False.
+        """
         self.reward_weights = reward.astype(np.float32)
         world = ThreeLaneCarWorld()
 
-        self.random_start = random_start
+        self.time_in_state = time_in_state
 
-        self.random_start_space = Box(
+        car_space = gym.spaces.Box(
+            low=np.array([float("-inf"), float("-inf"), 0, -1], dtype=np.float32),
+            high=np.array([float("inf"), float("inf"), 2 * np.pi, 1]),
+        )
+
+        if time_in_state:
+            self.observation_space = gym.spaces.Tuple(
+                car_space, gym.spaces.Discrete(self.HORIZON + 1)
+            )
+
+        self.random_start = random_start
+        self.random_start_space = gym.spaces.Box(
             low=np.array([-3 * world.lane_width, -0.6, 0, -1], dtype=np.float32),
             high=np.array([3 * world.lane_width, 0.3, 2 * np.pi, 2], dtype=np.float32),
         )
@@ -41,17 +61,47 @@ class LegacyEnv(CarEnv):
             init_state=np.array([world.lane_width, 0.0, np.pi / 2.0, 0.41], dtype=np.float32),
         )
         world.add_cars([self.main_car, other_car])
-        super().__init__(world, main_car_index=0)
+
+        # Composition instead of inheritance because the state signature is different
+        self.car_env = CarEnv(world)
+        self.action_space = self.car_env.action_space
 
         self.t = 0
 
-    def step(self, action: np.ndarray) -> Tuple[np.ndarray, float, bool, Dict]:
+    @property
+    def reward(self) -> np.ndarray:
+        return self.reward_weights
+
+    @reward.setter
+    def reward(self, reward: np.ndarray) -> None:
+        self.reward_weights = reward
+        self.main_car.weights = reward
+
+    @property
+    def state(self) -> np.ndarray:
+        return self.car_env.state
+
+    @state.setter
+    def state(self, state: Union[np.ndarray, Tuple[np.ndarray, int]]) -> None:
+        if isinstance(state, tuple):
+            car_state, time_remaining = state
+            self.t = self.HORIZON - time_remaining
+            self.car_env.state = car_state
+        else:
+            self.car_env.state = state
+
+    def step(
+        self, action: np.ndarray
+    ) -> Tuple[Union[np.ndarray, Tuple[np.ndarray, int]], float, bool, Dict]:
         action = action.astype(np.float32)
-        state, reward, _, info = super().step(action)
-        info["reward_features"] = self.main_car.features(state, action).numpy()
+        car_state, reward, _, info = self.car_env.step(action)
+        info["reward_features"] = self.main_car.features(car_state, action).numpy()
 
         self.t += 1
-        done = self.t >= 50
+        done = self.t >= self.HORIZON
+
+        if self.time_in_state:
+            state = (car_state, self.HORIZON - self.t)
 
         return state, reward, done, info
 
@@ -61,6 +111,31 @@ class LegacyEnv(CarEnv):
         if self.random_start:
             self.main_car.init_state = self.random_start_space.sample()
 
+        return self.car_env.reset()
+
+
+class RandomLegacyEnv(LegacyEnv):
+    """Legacy environment, except the reward weigths are randomized each episode"""
+
+    REWARD_SHAPE: Tuple[int] = (4,)
+
+    def __init__(self, random_start: bool = False, time_in_state: bool = False) -> None:
+        super().__init__(
+            reward=self.generate_reward(), random_start=random_start, time_in_state=time_in_state
+        )
+
+    def generate_reward(self) -> np.ndarray:
+        return safe_normalize(np.random.random(self.REWARD_SHAPE))
+
+    def step(
+        self, action: np.ndarray
+    ) -> Tuple[Union[np.ndarray, Tuple[np.ndarray, int]], float, bool, Dict]:
+        state, reward, done, info = super().step(action)
+        info["reward_weights"] = self.reward_weights
+        return state, reward, done, info
+
+    def reset(self) -> np.ndarray:
+        self.reward_weights = self.generate_reward()
         return super().reset()
 
 
